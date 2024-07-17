6e38bc7a4e6dddf061f5ac0071f532793294921f
William Bispo <william.oliveira@geap.com.br>
2024-07-17 16:59:43 -0300

    Initial entities

---
+++ src/rental.Domain/Entities/Realty.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rental.Domain.Entities;
+
+public class Realty : BaseEntity
+{
+    public decimal Price { get; set; }
+    public short NumberRooms { get; set; }
+    public short NumberSuites { get; set; }
+    public bool Condominium { get; set; }
+    public bool GarageSpace { get; set; }
+    public bool Status { get; set; }
+
+    public Adress Adress { get; set; } = new Adress();
+}
--- src/rental.Persistence/Context/RentalContext.cs
+++ src/rental.Persistence/Context/RentalContext.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,6 +7,10 @@ using System.Threading.Tasks;
 
 namespace rental.Persistence.Context;
 
-public class RentalContext
+public class RentalContext : DbContext
 {
+    public RentalContext(DbContextOptions<RentalContext> options) : base(options)
+    {
+        
+    }
 }

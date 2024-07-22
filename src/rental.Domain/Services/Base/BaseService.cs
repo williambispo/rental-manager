257659d88dd405f03f8d91b3306696520d994216
William Bispo <william.oliveira@geap.com.br>
2024-07-22 16:31:28 -0300

    Base Service

---
+++ src/rental.Domain/Services/Base/BaseService.cs
@@ -0,0 +1,45 @@
+﻿using rental.Domain.Entities;
+using rental.Domain.Interfaces.Repositories;
+using rental.Domain.Services.Base.Interface;
+using rental.Dto.Base;
+
+namespace rental.Domain.Services.Base;
+
+public class BaseService<TEntity, TEntityResponse, TEntityInsertRequest, TEntityUpdateRequest> : IBaseService<TEntity, TEntityResponse, TEntityInsertRequest, TEntityUpdateRequest>
+where TEntity : BaseEntity
+where TEntityResponse : BaseResponse
+where TEntityInsertRequest : BaseRequest
+where TEntityUpdateRequest : BaseRequest
+{
+    private readonly IBaseRepository<TEntity> _rep;
+
+    public BaseService(IBaseRepository<TEntity> rep)
+    {
+        _rep = rep;
+    }
+
+    public async Task<List<TEntityResponse>> ListAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<TEntityResponse> GetById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<TEntity> InsertAsync(TEntityInsertRequest request)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> UpdateAsync(TEntityUpdateRequest entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> Remove(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ src/rental.Domain/Services/Realty/Interface/IRealtyService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rental.Domain.Services.Realty.Interface;
+
+public interface IRealtyService
+{
+}
